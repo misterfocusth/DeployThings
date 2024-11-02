75e966cfab5544d07b56628852b01e014011f46b
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2024-11-02 19:49:22 +0700

    refactor(backend): ecr libs

--- apps/backend/src/libs/aws/ecr.ts
+++ apps/backend/src/libs/aws/ecr.ts
@@ -11,99 +11,93 @@ import * as util from "node:util";
 import {exec} from "node:child_process";
 import {pullImageFromDockerHub} from "../docker/docker";
 
-const credentials = getDefaultCredentials()
-const client = new ECRClient({region: CONSTANT.AWS_REGION, credentials: credentials})
-
+const client = new ECRClient({region: CONSTANT.AWS_REGION, credentials: getDefaultCredentials()});
 const execAsync = util.promisify(exec);
 
-const getRepositoryName = (username: string, imageName: string) => {
-    return `deploythings/${username}/${imageName}`
-}
-
-export const createECRRepository = async (username: string, imageName: string) => {
-    try {
-        const repositoryName = getRepositoryName(username, imageName)
-        const createCommand = new CreateRepositoryCommand({
-            repositoryName: repositoryName,
-        });
-
-        return await client.send(createCommand);
-    } catch (error) {
-        console.log(error)
-        throw error
-    }
-}
+const getRepositoryName = (username: string, imageName: string): string => `deploythings/${username}/${imageName}`;
 
 const authenticateECR = async () => {
-    const authTokenCommand = new GetAuthorizationTokenCommand({})
-    const authTokenResponse = await client.send(authTokenCommand)
+    const authTokenCommand = new GetAuthorizationTokenCommand({});
+    const authTokenResponse = await client.send(authTokenCommand);
 
-    if (authTokenResponse.authorizationData && authTokenResponse.authorizationData.length > 0) {
-        const {authorizationToken, proxyEndpoint} = authTokenResponse.authorizationData[0]
+    const authData = authTokenResponse.authorizationData?.[0];
+    if (!authData) throw new Error("No authorization data found");
 
-        const decodedAuth = Buffer.from(authorizationToken!, 'base64').toString()
-        const [username, password] = decodedAuth.split(':');
+    const decodedAuth = Buffer.from(authData.authorizationToken!, "base64").toString();
+    const [username, password] = decodedAuth.split(":");
 
-        await execAsync(`docker login -u ${username} -p ${password} ${proxyEndpoint}`);
-    } else {
-        throw new Error('No authorization data found')
-    }
-}
+    await execAsync(`docker login -u ${username} -p ${password} ${authData.proxyEndpoint}`);
+};
 
-async function logoutFromECR(repositoryUri: string) {
+const logoutFromECR = async (repositoryUri: string) => {
     await execAsync(`docker logout ${repositoryUri}`);
-}
+};
 
-const getECRRepositoryDetails = async (repositoryName: string) => {
+export const createECRRepository = async (username: string, imageName: string) => {
+    const repositoryName = getRepositoryName(username, imageName);
+    const createCommand = new CreateRepositoryCommand({repositoryName});
+
     try {
-        const command = new DescribeImagesCommand({
-            repositoryName,
-            imageIds: [{imageTag: 'latest'}]
-        });
-        const response = await client.send(command);
-
-        if (response.imageDetails && response.imageDetails.length > 0) {
-            return response.imageDetails[0];
-        } else {
-            return null;
-        }
+        return await client.send(createCommand);
     } catch (error) {
-        console.error('Error retrieving image details:', error);
+        console.error("Error creating ECR repository:", error);
         throw error;
     }
-}
+};
 
-export const uploadImageToECR = async (config: {
-    imageName: string,
-    repositoryName: string,
-    repositoryUri: string,
-    dockerHubAccessToken?: string
-}) => {
-    const {imageName, repositoryName, repositoryUri, dockerHubAccessToken} = config
+const getECRImageDetails = async (repositoryName: string, imageTag = "latest") => {
+    const describeImagesCommand = new DescribeImagesCommand({
+        repositoryName,
+        imageIds: [{imageTag}],
+    });
 
-    await pullImageFromDockerHub(imageName, dockerHubAccessToken)
-    await authenticateECR()
-    
-    await execAsync(`docker tag ${imageName}:latest ${repositoryUri}:latest`);
-    await execAsync(`docker push ${repositoryUri}:latest`);
+    try {
+        const response = await client.send(describeImagesCommand);
+        return response.imageDetails?.[0] || null;
+    } catch (error) {
+        console.error("Error retrieving image details:", error);
+        throw error;
+    }
+};
 
-    await logoutFromECR(repositoryUri)
+export const uploadImageToECR = async (
+    {
+        imageName,
+        repositoryName,
+        repositoryUri,
+        dockerHubAccessToken
+    }: {
+        imageName: string;
+        repositoryName: string;
+        repositoryUri: string;
+        dockerHubAccessToken?: string;
+    }) => {
+    try {
+        await pullImageFromDockerHub(imageName, dockerHubAccessToken);
+        await authenticateECR();
 
-    const imageDetails = await getECRRepositoryDetails(repositoryName)
-
-    if (!imageDetails) {
-        throw new Error(`Image ${imageName} not found in repository ${repositoryUri}`)
+        await execAsync(`docker tag ${imageName}:latest ${repositoryUri}:latest`);
+        await execAsync(`docker push ${repositoryUri}:latest`);
+    } finally {
+        await logoutFromECR(repositoryUri);
     }
 
-    return imageDetails
-}
+    const imageDetails = await getECRImageDetails(repositoryName);
+    if (!imageDetails) {
+        throw new Error(`Image ${imageName} not found in repository ${repositoryUri}`);
+    }
+
+    return imageDetails;
+};
 
 export const listECRRepositories = async () => {
-    const command = new DescribeRepositoriesCommand({});
+    const describeRepositoriesCommand = new DescribeRepositoriesCommand({});
+
     try {
-        return await client.send(command)
+        const response = await client.send(describeRepositoriesCommand);
+        return response.repositories;
     } catch (error) {
-        console.error(error)
-        throw error
+        console.error("Error listing ECR repositories:", error);
+        throw error;
     }
-}
+};
