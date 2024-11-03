7358f5f79a344d852e0a3f6c90f5dc83291d8bfc
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2024-11-04 00:54:37 +0700

    feat(backend): add ecs service list / create / delete function

--- apps/backend/src/libs/aws/ecs.ts
+++ apps/backend/src/libs/aws/ecs.ts
@@ -1,12 +1,18 @@
 import {
+  CreateClusterCommand,
+  CreateServiceCommand,
+  DeleteServiceCommand,
   DeregisterTaskDefinitionCommand,
   ECSClient,
   ListTaskDefinitionsCommand,
   RegisterTaskDefinitionCommand,
   RegisterTaskDefinitionCommandInput,
+  UpdateServiceCommand,
+  waitUntilServicesInactive,
 } from "@aws-sdk/client-ecs";
 import { CONSTANT } from "../../../constant";
 import { getDefaultCredentials } from "./credentials";
+import { Cluster, Service, TaskDefinition } from "@prisma/client";
 
 const credentials = getDefaultCredentials();
 const ecsClient = new ECSClient({ region: CONSTANT.AWS_REGION, credentials: credentials });
@@ -47,6 +53,113 @@ export const deleteECSTaskDefinition = async (taskDefinitionFamilyName: string) 
     }
   } catch (error) {
     console.error("Error deleting ECS Task Definition", error);
+    throw error;
+  }
+};
+
+export const createECSCluster = async ({ clusterName }: { clusterName: string }) => {
+  const command = new CreateClusterCommand({
+    clusterName,
+    capacityProviders: ["FARGATE_SPOT", "FARGATE"],
+  });
+
+  try {
+    return await ecsClient.send(command);
+  } catch (error) {
+    console.error("Error creating ECS Cluster", error);
+    throw error;
+  }
+};
+
+export const createECSService = async ({
+  serviceName,
+  cluster,
+  taskDefinition,
+  providerBase,
+  providerWeight,
+}: {
+  serviceName: string;
+  cluster: Cluster;
+  taskDefinition: TaskDefinition;
+  providerBase: number;
+  providerWeight: number;
+}) => {
+  if (!CONSTANT.AWS_SUBNETS[0] || !CONSTANT.AWS_SUBNETS[1] || !CONSTANT.AWS_SUBNETS[2]) {
+    throw new Error("AWS_SUBNETS are not set");
+  }
+
+  if (!CONSTANT.AWS_SECURITY_GROUP_ID) {
+    throw new Error("AWS_SECURITY_GROUP_ID is not set");
+  }
+
+  const command = new CreateServiceCommand({
+    cluster: cluster.arn,
+    serviceName: serviceName,
+    taskDefinition: taskDefinition.arn,
+    desiredCount: 1,
+    capacityProviderStrategy: [
+      // {
+      //   capacityProvider: "FARGATE_SPOT",
+      //   base: providerBase,
+      //   weight: providerWeight,
+      // },
+      {
+        capacityProvider: "FARGATE",
+        base: providerBase,
+        weight: providerWeight,
+      },
+    ],
+    networkConfiguration: {
+      awsvpcConfiguration: {
+        subnets: [CONSTANT.AWS_SUBNETS[0], CONSTANT.AWS_SUBNETS[1], CONSTANT.AWS_SUBNETS[2]],
+        securityGroups: [CONSTANT.AWS_SECURITY_GROUP_ID],
+        assignPublicIp: "ENABLED",
+      },
+    },
+    launchType: "FARGATE",
+  });
+
+  try {
+    return await ecsClient.send(command);
+  } catch (error) {
+    console.error("Error creating ECS Service", error);
+    throw error;
+  }
+};
+
+export const deleteEcsService = async ({
+  cluster,
+  service,
+}: {
+  cluster: Cluster;
+  service: Service;
+}) => {
+  const clusterArn = cluster.arn;
+  const serviceArn = service.arn;
+
+  try {
+    await ecsClient.send(
+      new UpdateServiceCommand({
+        cluster: clusterArn,
+        service: serviceArn,
+        desiredCount: 0,
+      })
+    );
+
+    await waitUntilServicesInactive(
+      { client: ecsClient, maxWaitTime: 1000 },
+      { cluster: clusterArn, services: [serviceArn] }
+    );
+
+    await ecsClient.send(
+      new DeleteServiceCommand({
+        cluster: clusterArn,
+        service: serviceArn,
+        force: true,
+      })
+    );
+  } catch (error) {
+    console.error("Error deleting ECS service:", error);
     throw error;
   }
 };
---
+++ packages/contracts/routes/service.ts
@@ -0,0 +1,76 @@
+import { initContract } from "@ts-rest/core";
+import { z } from "zod";
+import { baseSchema, ErrorResponse } from "../schemas";
+import { userSchema } from "../schemas/user";
+import { taskSchema } from "./task";
+
+const c = initContract();
+
+export const clusterSchema = baseSchema.extend({
+  arn: z.string(),
+  name: z.string(),
+  namespace: z.string(),
+  infrastructure: z.string(),
+  ownerId: z.string(),
+  owner: userSchema,
+});
+
+export const serviceSchema = baseSchema.extend({
+  arn: z.string(),
+  name: z.string(),
+  publicIP: z.string(),
+  publicPort: z.number(),
+  providerBase: z.number().default(1),
+  providerWeight: z.number().default(1),
+  taskDefinition: taskSchema,
+  taskDefinitionId: z.string(),
+  cluster: clusterSchema,
+  clusterId: z.string(),
+});
+
+type Service = z.infer<typeof serviceSchema>;
+
+const listServiceResponse = serviceSchema.array();
+type ListServiceResponse = z.infer<typeof listServiceResponse>;
+
+export const ServiceContract = c.router({
+  listServices: {
+    method: "GET",
+    path: "/services",
+    responses: {
+      200: c.type<ListServiceResponse>(),
+      404: c.type<ErrorResponse>(),
+      500: c.type<ErrorResponse>(),
+    },
+    summary: "List services",
+  },
+  createService: {
+    method: "POST",
+    path: "/services",
+    responses: {
+      200: c.type<Service>(),
+      400: c.type<ErrorResponse>(),
+      404: c.type<ErrorResponse>(),
+      500: c.type<ErrorResponse>(),
+    },
+    body: z.object({
+      serviceName: z.string(),
+      userId: z.string(),
+      taskDefinitionId: z.string(),
+      providerBase: z.number().default(1),
+      providerWeight: z.number().default(1),
+    }),
+    summary: "Create service",
+  },
+  deleteService: {
+    method: "DELETE",
+    path: "/services/:id",
+    responses: {
+      204: c.type<null>(),
+      404: c.type<ErrorResponse>(),
+      500: c.type<ErrorResponse>(),
+    },
+    body: z.any().nullable(),
+    summary: "Delete service",
+  },
+});
