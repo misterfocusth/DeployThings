20d507cb132f3c4368222d72cc08440d3b893601
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2024-11-04 16:30:20 +0700

    refactor(backend): remove unused console.log()

--- apps/backend/src/routes/postImpl.ts
+++ apps/backend/src/routes/postImpl.ts
@@ -1,31 +1,30 @@
-import {type AppRouteImplementation} from "@ts-rest/express";
+import { type AppRouteImplementation } from "@ts-rest/express";
 
 // Contracts
-import {contract} from "@repo/contracts";
+import { contract } from "@repo/contracts";
 
-export const getPost: AppRouteImplementation<typeof contract.post.getPost> = async ({
-  params: { id },
-}) => {
-    console.log(id)
-    const posts = [
-        {
-            id: "1",
-            title: "Sample Post",
-            body: "This is a sample post",
-        },
-        {
-            id: "2",
-            title: "Sample Post 2",
-            body: "This is a sample post 2",
-        }
-    ]
+export const getPost: AppRouteImplementation<typeof contract.post.getPost> = async () => {
+  const posts = [
+    {
+      id: "1",
+      title: "Sample Post",
+      body: "This is a sample post",
+    },
+    {
+      id: "2",
+      title: "Sample Post 2",
+      body: "This is a sample post 2",
+    },
+  ];
   return {
     status: 200,
-      body: posts
+    body: posts,
   };
 };
 
-export const createPost: AppRouteImplementation<typeof contract.post.createPost> = async ({body}) => {
+export const createPost: AppRouteImplementation<typeof contract.post.createPost> = async ({
+  body,
+}) => {
   return {
     status: 201,
     body: { ...body, id: "123" },
